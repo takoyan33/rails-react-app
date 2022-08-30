429e31a5df9dfa876be13cb37d2adfff834e9d85
takoyan33 <harrier2070@gmail.com>
2022-08-30 18:31:55 +0900

    darkmodeの追加

--- frontend/app/src/components/Darkmode.tsx
+++ frontend/app/src/components/Darkmode.tsx
@@ -1,28 +1,48 @@
-import { Container, CssBaseline } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import React from "react";
-
-//const theme = createTheme();　※フックを使うのでfunction内でcreateThemeする必要がある
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import * as colors from "@material-ui/core/colors";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import IconButton from "@material-ui/core/IconButton";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
 
 export default function SignIn() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-        },
-      }),
-    [prefersDarkMode]
+  const [darkMode, setDarkMode] = React.useState(
+    localStorage.getItem("darkMode") === "on" ? true : false
   );
+  const handleDarkModeOn = () => {
+    localStorage.setItem("darkMode", "on");
+    setDarkMode(true);
+  };
+  const handleDarkModeOff = () => {
+    localStorage.setItem("darkMode", "off");
+    setDarkMode(false);
+  };
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: colors.blue[800],
+      },
+      type: darkMode ? "dark" : "light",
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <div>
         <CssBaseline />
-        <p>ライトモードに変更</p>
-      </Container>
+        {darkMode ? (
+          <IconButton color="inherit" onClick={handleDarkModeOff}>
+            <Brightness7Icon />
+            ホワイト
+          </IconButton>
+        ) : (
+          <IconButton color="inherit" onClick={handleDarkModeOn}>
+            <Brightness4Icon />
+            ダーク
+          </IconButton>
+        )}
+      </div>
     </ThemeProvider>
   );
 }
--- frontend/app/src/pages/ResasPage.jsx
+++ frontend/app/src/pages/ResasPage.jsx
@@ -3,25 +3,33 @@ import axios from "axios";
 
 const Resas = () => {
   const [resass, setResas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const resassurl = "https://opendata.resas-portal.go.jp/api/v1";
 
   useEffect(() => {
+
     // 都道府県一覧を取得する
     axios
-      .get("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      .get(`${resassurl}/prefectures`, {
         headers: { "X-API-KEY": "GekHJw2h9l1K2N03H0yDPRBzYhmUDlcooBGam8QQ" },
       })
       .then((results) => {
         setResas(results.data.result);
+        setLoading(false);
       })
       .catch((error) => {});
   }, []);
 
   console.log(resass);
 
+  
+
   return (
     <>
       <h2>Resas</h2>
       <p>県の名前</p>
+      {loading == true && <p>loding中です。</p>}
+
       {resass.map((el) => (
         <li key={el.prefCode}> {el.prefName} </li>
       ))}
